383709d07b5366a42394c08e96632718142b3d1c
Ed <ed@polarity.io>
2019-11-11 21:45:17 -0500

    Update formatting

--- integration.js
+++ integration.js
@@ -1,57 +1,44 @@
-"use strict";
+'use strict';
 
-const request = require("request");
-const _ = require("lodash");
-const async = require("async");
-const config = require("./config/config");
-const fs = require("fs");
+const request = require('request');
+const _ = require('lodash');
+const async = require('async');
+const config = require('./config/config');
+const fs = require('fs');
 
 let Logger;
 let requestWithDefaults;
-let previousDomainRegexAsString = "";
-let previousIpRegexAsString = "";
+let previousDomainRegexAsString = '';
+let previousIpRegexAsString = '';
 let domainBlacklistRegex = null;
 let ipBlacklistRegex = null;
 
-const BASE_URI = "https://api.intelgraph.idefense.com/rest/fundamental/v0/";
+const BASE_URI = 'https://api.intelgraph.idefense.com/rest/fundamental/v0/';
 const MAX_DOMAIN_LABEL_LENGTH = 256;
 const MAX_ENTITY_LENGTH = 100;
 
-
 function _setupRegexBlacklists(options) {
-  if (
-    options.domainBlacklistRegex !== previousDomainRegexAsString &&
-    options.domainBlacklistRegex.length === 0
-  ) {
-    Logger.debug("Removing Domain Blacklist Regex Filtering");
-    previousDomainRegexAsString = "";
+  if (options.domainBlacklistRegex !== previousDomainRegexAsString && options.domainBlacklistRegex.length === 0) {
+    Logger.debug('Removing Domain Blacklist Regex Filtering');
+    previousDomainRegexAsString = '';
     domainBlacklistRegex = null;
   } else {
     if (options.domainBlacklistRegex !== previousDomainRegexAsString) {
       previousDomainRegexAsString = options.domainBlacklistRegex;
-      Logger.debug(
-        { domainBlacklistRegex: previousDomainRegexAsString },
-        "Modifying Domain Blacklist Regex"
-      );
-      domainBlacklistRegex = new RegExp(options.domainBlacklistRegex, "i");
+      Logger.debug({ domainBlacklistRegex: previousDomainRegexAsString }, 'Modifying Domain Blacklist Regex');
+      domainBlacklistRegex = new RegExp(options.domainBlacklistRegex, 'i');
     }
   }
 
-  if (
-    options.ipBlacklistRegex !== previousIpRegexAsString &&
-    options.ipBlacklistRegex.length === 0
-  ) {
-    Logger.debug("Removing IP Blacklist Regex Filtering");
-    previousIpRegexAsString = "";
+  if (options.ipBlacklistRegex !== previousIpRegexAsString && options.ipBlacklistRegex.length === 0) {
+    Logger.debug('Removing IP Blacklist Regex Filtering');
+    previousIpRegexAsString = '';
     ipBlacklistRegex = null;
   } else {
     if (options.ipBlacklistRegex !== previousIpRegexAsString) {
       previousIpRegexAsString = options.ipBlacklistRegex;
-      Logger.debug(
-        { ipBlacklistRegex: previousIpRegexAsString },
-        "Modifying IP Blacklist Regex"
-      );
-      ipBlacklistRegex = new RegExp(options.ipBlacklistRegex, "i");
+      Logger.debug({ ipBlacklistRegex: previousIpRegexAsString }, 'Modifying IP Blacklist Regex');
+      ipBlacklistRegex = new RegExp(options.ipBlacklistRegex, 'i');
     }
   }
 }
@@ -64,32 +51,29 @@ function doLookup(entities, options, cb) {
   async.each(
     entities,
     function(entityObj, next) {
-        if (
-          _isInvalidEntity(entityObj) ||
-          _isEntityBlacklisted(entityObj, options)
-        ) {
-          next(null);
-        } else if (entityObj.types.indexOf('custom.cpe') >= 0 ) {
-          _lookupEntityCPE(entityObj, options, function(err, result) {
-            if (err) {
-              next(err);
-            } else {
-              lookupResults.push(result);
-              //Logger.debug({ result: result }, "Checking the result values");
-              next(null);
-            }
-          });
-        }else {
-          _lookupEntity(entityObj, options, function(err, result) {
-            if (err) {
-              next(err);
-            } else {
-              lookupResults.push(result);
-              //Logger.debug({ result: result }, "Checking the result values");
-              next(null);
-            }
-          });
-        }
+      if (_isInvalidEntity(entityObj) || _isEntityBlacklisted(entityObj, options)) {
+        next(null);
+      } else if (entityObj.types.indexOf('custom.cpe') >= 0) {
+        _lookupEntityCPE(entityObj, options, function(err, result) {
+          if (err) {
+            next(err);
+          } else {
+            lookupResults.push(result);
+            //Logger.debug({ result: result }, "Checking the result values");
+            next(null);
+          }
+        });
+      } else {
+        _lookupEntity(entityObj, options, function(err, result) {
+          if (err) {
+            next(err);
+          } else {
+            lookupResults.push(result);
+            //Logger.debug({ result: result }, "Checking the result values");
+            next(null);
+          }
+        });
+      }
     },
     function(err) {
       cb(err, lookupResults);
@@ -105,11 +89,11 @@ function _isInvalidEntity(entityObj) {
 
   // Domain labels (the parts in between the periods, must be 63 characters or less
   if (entityObj.isDomain) {
-    const invalidLabel = entityObj.value.split(".").find(label => {
+    const invalidLabel = entityObj.value.split('.').find((label) => {
       return label.length > 256;
     });
 
-    if (typeof invalidLabel !== "undefined") {
+    if (typeof invalidLabel !== 'undefined') {
       return true;
     }
   }
@@ -120,10 +104,7 @@ function _isInvalidEntity(entityObj) {
 function _isEntityBlacklisted(entityObj, options) {
   const blacklist = options.blacklist;
 
-  Logger.trace(
-    { blacklist: blacklist },
-    "checking to see what blacklist looks like"
-  );
+  Logger.trace({ blacklist: blacklist }, 'checking to see what blacklist looks like');
 
   if (_.includes(blacklist, entityObj.value.toLowerCase())) {
     return true;
@@ -132,7 +113,7 @@ function _isEntityBlacklisted(entityObj, options) {
   if (entityObj.isIPv4 && !entityObj.isPrivateIP) {
     if (ipBlacklistRegex !== null) {
       if (ipBlacklistRegex.test(entityObj.value)) {
-        Logger.debug({ ip: entityObj.value }, "Blocked BlackListed IP Lookup");
+        Logger.debug({ ip: entityObj.value }, 'Blocked BlackListed IP Lookup');
         return true;
       }
     }
@@ -141,10 +122,7 @@ function _isEntityBlacklisted(entityObj, options) {
   if (entityObj.isDomain) {
     if (domainBlacklistRegex !== null) {
       if (domainBlacklistRegex.test(entityObj.value)) {
-        Logger.debug(
-          { domain: entityObj.value },
-          "Blocked BlackListed Domain Lookup"
-        );
+        Logger.debug({ domain: entityObj.value }, 'Blocked BlackListed Domain Lookup');
         return true;
       }
     }
@@ -158,31 +136,31 @@ function _getUrl(entityObj, options) {
   let entityValue = entityObj.value.toLowerCase();
   // map entity object type to the IRIS REST API type
   switch (entityObj.type) {
-    case "domain":
-      entityType = "domain";
-      query= "key.query";
+    case 'domain':
+      entityType = 'domain';
+      query = 'key.query';
       break;
-    case "IPv4":
-      entityType = "ip";
-      query= "key.query";
+    case 'IPv4':
+      entityType = 'ip';
+      query = 'key.query';
       break;
-    case "email":
-      entityType = "phish";
-      query= "sender.query";
+    case 'email':
+      entityType = 'phish';
+      query = 'sender.query';
       entityValue = `"${entityValue}"`;
       break;
-    case "hash":
-      entityType = "file";
-      query= "key.query";
+    case 'hash':
+      entityType = 'file';
+      query = 'key.query';
       break;
-    case "custom":
-      entityType = "vulnerability";
-      query= "key.values";
-      entityValue=entityObj.value.toUpperCase();
+    case 'custom':
+      entityType = 'vulnerability';
+      query = 'key.values';
+      entityValue = entityObj.value.toUpperCase();
       break;
-    case "url":
-      entityType = "url";
-      query= "key.query";
+    case 'url':
+      entityType = 'url';
+      query = 'key.query';
       break;
   }
   let request = {
@@ -197,15 +175,14 @@ function _getUrl(entityObj, options) {
   return request;
 }
 
-
 function _getRequestOptions(entityObj, options) {
   let request = _getUrl(entityObj, options);
 
   return {
     uri: request.uri,
-    headers: {"auth-token": options.apiKey},
+    headers: { 'auth-token': options.apiKey },
     qs: request.qs,
-    method: "GET",
+    method: 'GET',
     json: true
   };
 }
@@ -216,24 +193,24 @@ function _lookupEntity(entityObj, options, cb) {
 
   let minScore = parseInt(options.minScore, 10);
 
-  Logger.trace({options: requestOptions}, "Checking the request options coming through");
+  Logger.trace({ options: requestOptions }, 'Checking the request options coming through');
   let url = null;
 
-  if(entityObj.type === "IPv4"){
-    url = "https://intelgraph.idefense.com/#/node/ip/view/";
-  } else if (entityObj.type === "domain"){
-    url = "https://intelgraph.idefense.com/#/node/domain/view/";
-  } else if (entityObj.type === "url"){
-    url = "https://intelgraph.idefense.com/#/node/url/view/";
-  } else if (entityObj.type === "hash"){
-    url = "https://intelgraph.idefense.com/#/node/file/view/";
-  } else if (entityObj.types.indexOf('custom.cve') >= 0){
-    url = "https://intelgraph.idefense.com/#/node/vulnerability/view/";
-  }else {
-    url = "https://intelgraph.idefense.com/#/node/phish/view/";
+  if (entityObj.type === 'IPv4') {
+    url = 'https://intelgraph.idefense.com/#/node/ip/view/';
+  } else if (entityObj.type === 'domain') {
+    url = 'https://intelgraph.idefense.com/#/node/domain/view/';
+  } else if (entityObj.type === 'url') {
+    url = 'https://intelgraph.idefense.com/#/node/url/view/';
+  } else if (entityObj.type === 'hash') {
+    url = 'https://intelgraph.idefense.com/#/node/file/view/';
+  } else if (entityObj.types.indexOf('custom.cve') >= 0) {
+    url = 'https://intelgraph.idefense.com/#/node/vulnerability/view/';
+  } else {
+    url = 'https://intelgraph.idefense.com/#/node/phish/view/';
   }
 
-  Logger.trace({url}, 'Lookup URL');
+  Logger.trace({ url }, 'Lookup URL');
 
   //const researchUri = LOOKUP_URI + entityObj.value;
   requestWithDefaults(requestOptions, function(err, response, body) {
@@ -271,7 +248,7 @@ function _lookupEntity(entityObj, options, cb) {
 
     let score = scores[0];
 
-    if(score < minScore){
+    if (score < minScore) {
       cb(null, {
         entity: entityObj,
         data: null // this entity will be cached as a miss
@@ -298,22 +275,18 @@ function _lookupEntity(entityObj, options, cb) {
 }
 
 function _lookupEntityCPE(entityObj, options, cb) {
-
   let minScore = parseInt(options.minScore, 10);
   //Logger.trace("Logging if Running");
   const requestOptions = {
-    uri: BASE_URI + "vuln_tech?key.query=" + entityObj.value.toLowerCase() + "&page_size="+ options.pageSize,
-  headers: {"auth-token": options.apiKey},
-  method: "GET",
-  json: true
+    uri: BASE_URI + 'vuln_tech?key.query=' + entityObj.value.toLowerCase() + '&page_size=' + options.pageSize,
+    headers: { 'auth-token': options.apiKey },
+    method: 'GET',
+    json: true
+  };
 
-  }
+  Logger.trace({ options: requestOptions }, 'Checking the request options coming through');
 
-  Logger.trace({options: requestOptions}, "Checking the request options coming through");
-
-  let url = "https://intelgraph.idefense.com/#/node/cpe/view/"
-
-
+  let url = 'https://intelgraph.idefense.com/#/node/cpe/view/';
 
   //const researchUri = LOOKUP_URI + entityObj.value;
   requestWithDefaults(requestOptions, function(err, response, body) {
@@ -323,7 +296,6 @@ function _lookupEntityCPE(entityObj, options, cb) {
       return;
     }
 
-
     if (_isLookupMiss(response, body)) {
       return cb(null, {
         entity: entityObj,
@@ -331,10 +303,7 @@ function _lookupEntityCPE(entityObj, options, cb) {
       });
     }
 
-    Logger.debug(
-      { body: body, entity: entityObj.value },
-      "Printing out the results of Body "
-    );
+    Logger.debug({ body: body, entity: entityObj.value }, 'Printing out the results of Body ');
 
     if (_.isNull(body) || _.isEmpty(body) || body.total_size === 0) {
       cb(null, {
@@ -352,14 +321,13 @@ function _lookupEntityCPE(entityObj, options, cb) {
 
     let score = scores[0];
 
-    if(score < minScore){
+    if (score < minScore) {
       cb(null, {
         entity: entityObj,
         data: null // this entity will be cached as a miss
       });
       return;
     }
-
 
     // The lookup results returned is an array of lookup objects with the following format
     cb(null, {
@@ -386,14 +354,14 @@ function _isLookupMiss(response, body) {
     response.statusCode === 503 ||
     response.statusCode === 300 ||
     response.statusCode === 500 ||
-    typeof body === "undefined"
+    typeof body === 'undefined'
   );
 }
 
 function _isApiError(err, response, body, entityValue) {
   if (err) {
     return {
-      detail: "Error executing HTTP request",
+      detail: 'Error executing HTTP request',
       error: err
     };
   }
@@ -408,24 +376,24 @@ function _isApiError(err, response, body, entityValue) {
     response.statusCode !== 500
   ) {
     return _createJsonErrorPayload(
-      "Unexpected HTTP Status Code",
+      'Unexpected HTTP Status Code',
       null,
       response.statusCode,
-      "1",
-      "Unexpected HTTP Status Code",
+      '1',
+      'Unexpected HTTP Status Code',
       {
         err: err,
         body: body,
         entityValue: entityValue
       }
     );
-  }else if (response.statusCode === 500){
+  } else if (response.statusCode === 500) {
     return _createJsonErrorPayload(
-      "Error with ApiKey",
+      'Error with ApiKey',
       null,
       response.statusCode,
-      "1",
-      "ApiKey is incorrect, please contact iDefense for correct key.",
+      '1',
+      'ApiKey is incorrect, please contact iDefense for correct key.',
       {
         err: err,
         body: body,
@@ -440,39 +408,32 @@ function _isApiError(err, response, body, entityValue) {
 function validateOptions(userOptions, cb) {
   let errors = [];
   if (
-    typeof userOptions.apiKey.value !== "string" ||
-    (typeof userOptions.apiKey.value === "string" &&
-      userOptions.apiKey.value.length === 0)
+    typeof userOptions.apiKey.value !== 'string' ||
+    (typeof userOptions.apiKey.value === 'string' && userOptions.apiKey.value.length === 0)
   ) {
     errors.push({
-      key: "apiKey",
-      message: "You must provide an iDefense API key"
+      key: 'apiKey',
+      message: 'You must provide an iDefense API key'
     });
   }
 
-  if (
-    typeof userOptions.domainBlacklistRegex.value === "string" &&
-    userOptions.domainBlacklistRegex.value.length > 0
-  ) {
+  if (typeof userOptions.domainBlacklistRegex.value === 'string' && userOptions.domainBlacklistRegex.value.length > 0) {
     try {
       new RegExp(userOptions.domainBlacklistRegex.value);
     } catch (error) {
       errors.push({
-        key: "domainBlacklistRegex",
+        key: 'domainBlacklistRegex',
         message: error.toString()
       });
     }
   }
 
-  if (
-    typeof userOptions.ipBlacklistRegex.value === "string" &&
-    userOptions.ipBlacklistRegex.value.length > 0
-  ) {
+  if (typeof userOptions.ipBlacklistRegex.value === 'string' && userOptions.ipBlacklistRegex.value.length > 0) {
     try {
       new RegExp(userOptions.ipBlacklistRegex.value);
     } catch (e) {
       errors.push({
-        key: "ipBlacklistRegex",
+        key: 'ipBlacklistRegex',
         message: error.toString()
       });
     }
@@ -494,7 +455,7 @@ function _createJsonErrorObject(msg, pointer, httpCode, code, title, meta) {
     detail: msg,
     status: httpCode.toString(),
     title: title,
-    code: "iDef_" + code.toString()
+    code: 'iDef_' + code.toString()
   };
 
   if (pointer) {
@@ -514,32 +475,23 @@ function startup(logger) {
   Logger = logger;
   let defaults = {};
 
-  if (
-    typeof config.request.cert === "string" &&
-    config.request.cert.length > 0
-  ) {
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
     defaults.cert = fs.readFileSync(config.request.cert);
   }
 
-  if (typeof config.request.key === "string" && config.request.key.length > 0) {
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
     defaults.key = fs.readFileSync(config.request.key);
   }
 
-  if (
-    typeof config.request.passphrase === "string" &&
-    config.request.passphrase.length > 0
-  ) {
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
     defaults.passphrase = config.request.passphrase;
   }
 
-  if (typeof config.request.ca === "string" && config.request.ca.length > 0) {
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
     defaults.ca = fs.readFileSync(config.request.ca);
   }
 
-  if (
-    typeof config.request.proxy === "string" &&
-    config.request.proxy.length > 0
-  ) {
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
     defaults.proxy = config.request.proxy;
   }
 
